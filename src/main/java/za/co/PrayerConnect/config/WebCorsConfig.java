7106886e91b206ef1d3920f1ce30163e4cb7a779
Ranelan <raneyclassic@gmail.com>
2025-06-23 00:33:48 +0200

    PrayerRequest controller working but has some small issues to consider before moving foward

---
+++ src/main/java/za/co/PrayerConnect/config/WebCorsConfig.java
@@ -0,0 +1,17 @@
+package za.co.PrayerConnect.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebCorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // allow all endpoints
+                .allowedOrigins("http://localhost:3000") // allow React frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
