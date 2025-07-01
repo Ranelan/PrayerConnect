5a00b58dbc23339b1ca8fc08ccbb064e870c8cce
Ranelan <raneyclassic@gmail.com>
2025-07-02 01:32:29 +0200

    Testimony Repository updated

--- src/main/java/za/co/PrayerConnect/repository/TestimonyRepository.java
+++ src/main/java/za/co/PrayerConnect/repository/TestimonyRepository.java
@@ -7,7 +7,8 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TestimonyRepository extends JpaRepository<Testimony, Long> {
-    Optional<Testimony> findByUserId(Long userId);
+    List<Testimony> findByUserId(Long userId);
+    List<Testimony> findByPrayerRequest_ContentId(Long contentId);
     // Additional query methods can be defined here if needed
     // For example, you could add methods to find testimonies by user or by approval status
     //List<Testimony> findAll();
