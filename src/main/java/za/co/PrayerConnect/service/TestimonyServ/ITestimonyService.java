e75159a1520618928d0cbbf5405414b3bd4cd648
Ranelan <raneyclassic@gmail.com>
2025-07-02 01:30:57 +0200

    Testimony added

--- src/main/java/za/co/PrayerConnect/service/TestimonyServ/ITestimonyService.java
+++ src/main/java/za/co/PrayerConnect/service/TestimonyServ/ITestimonyService.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 public interface ITestimonyService extends IService<Testimony, Long> {
     List<Testimony> findAll();
-    Optional<Testimony> findByUserId(Long userId);
-    Optional<Testimony> findByContentId(Long contentId);
+    List<Testimony> findByUserId(Long userId);
+    List<Testimony> findByPrayerRequest_ContentId(Long contentId);
+
 }
